4f80c51a7aa5805710e8728ea3985b14182782cb
Ceansyr <chinmaymandavkar@gmail.com>
2025-04-28 23:16:22 +0530

    Fix line ending issues

--- src/components/auth/ErrorMessage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ErrorMessage = ({ message }) => {
-  if (!message) return null;
-  
-  return <p className="error-message">{message}</p>;
-};
-
-export default ErrorMessage;
